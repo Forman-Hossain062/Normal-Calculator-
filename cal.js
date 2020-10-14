f4ae8b34930eee5c01fad1915c1ced6beadfc521
Forman Hossain <68132944+Forman-Hossain062@users.noreply.github.com>
2020-10-14 20:51:45 +0600

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,20 @@
+function insertNumber(number) {
+
+    let existingNumbers = $("#result").val()
+    $("#result").val(existingNumbers + number);
+}
+
+function clearResult() {
+    $("#result").val("")
+}
+
+function calculate() {
+    let result = eval($("#result").val())
+    $("#result").val(result);
+}
+function deleteNumber() {
+    let delLastNumber = $("#result").val();
+    if (delLastNumber != '') {
+        $("#result").val($("#result").val().slice(0, -1));
+    }
+}
